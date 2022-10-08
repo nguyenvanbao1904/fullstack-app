bd35caa47afa3f2a19e9772988007f0c412fddbd
nguyenvanbao <bao19042004@gmail.com>
2022-10-08 22:55:43 +0700

    fix path is not defined

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const helmet = require("helmet");
+const path = require("path");
 const port = 3000;
 
 app.use(cors());
@@ -11,10 +12,10 @@ app.get("/api", (req, res) => {
     res.json("api");
 });
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(port, function () {
     console.log(`server is running on port: ${port}`);
